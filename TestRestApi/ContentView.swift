8445c606add99289483dd95e73dde5a476eef2fc
kynmh69 <hiroki.kayanuma@gmail.com>
2020-04-01 20:47:26 +0900

    Initial Commit

---
+++ TestRestApi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestRestApi
+//
+//  Created by Hiroki Kayanuma on 2020/04/01.
+//  Copyright © 2020 Hiroki Kayanuma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
